5998758a8d8a65ae9507d7bd2d77a2f5b0e55191
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-20 17:11:13 +0900

    リストに追加するDOMを生成

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 'use strict'
 {
     const onClickAdd = () => {
+
+        // 入力されたテキストを取得＆初期化
         const inputText = document.getElementById('add-text').value;
         document.getElementById('add-text').value = "";
-        alert(inputText);
+
+        // リストに追加するため必要な要素を生成
+        const li = document.createElement("li");
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        div.className = "list-row";
+        p.innerText = inputText;
+
+        // 子要素の生成
+        li.appendChild(div);
+        div.appendChild(p);
+        console.log(li);
+
     }
 
     document.getElementById("add-button").addEventListener("click", () => onClickAdd());
